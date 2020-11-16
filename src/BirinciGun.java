acd48335a2a8ef7e41ceb37c09368e7bcc7215a7
Ebubekir <esahin3@icloud.com>
2020-11-17 00:00:29 +0300

    dorduncu commit

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -3,8 +3,12 @@ public class BirinciGun {
 
 		public static void main(String[]args) {
 			
-			
+			int a = 10;
 			System.out.println("11:05 - Birinci Gun");
+			System.out.println("qwEqwe");
+			System.out.println("qweqweöqweq");
+			System.out.println(a);
+			
 			
 		}
 }
