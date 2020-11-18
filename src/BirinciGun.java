05ec730a989bbe14166fcab1787e82ee301732db
Ebubekir <esahin3@icloud.com>
2020-11-18 22:27:56 +0300

    feature

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -12,12 +12,8 @@ public class BirinciGun {
 			System.out.println("11:05 - local");
 			System.out.println("11:23 - github.com");
 			System.out.println("ben buradan conflict beklemiyorum");
-<<<<<<< HEAD
-			System.out.println("localden yepyeni bir satir");
-=======
 			System.out.println("yepyeni bir satir");
-
->>>>>>> refs/remotes/origin/master
+			System.out.println("10:26 - feature branch");
 			
 
 		}
